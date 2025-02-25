22060e346ae4abe2938886d4c3c60f1cc0174060
OscarMG018 <108469203+OscarMG018@users.noreply.github.com>
2025-02-25 18:54:10 +0100

    commit

--- CamaraX/app/build.gradle.kts
+++ CamaraX/app/build.gradle.kts
@@ -1,13 +1,14 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.example.camaraxoscarmedina"
+    namespace = "com.example.camarax.oscar.medina.galvez"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.camaraxoscarmedina"
+        applicationId = "com.example.camarax.oscar.medina.galvez"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -25,21 +26,30 @@ android {
             )
         }
     }
+    buildFeatures {
+        viewBinding = true
+    }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
     }
 }
 
 dependencies {
-    implementation(libs.appcompat)
+    implementation("androidx.camera:camera-core:1.2.2")
+    implementation("androidx.camera:camera-camera2:1.2.2")
+    implementation("androidx.camera:camera-lifecycle:1.2.2")
+    implementation("androidx.camera:camera-view:1.2.2")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    implementation("androidx.camera:camera-camera2:1.0.0-beta07")
-    implementation("androidx.camera:camera-lifecycle:1.0.0-beta07")
-    implementation("androidx.camera:camera-view:1.0.0-alpha14")
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
 }
--- CamaraX/settings.gradle.kts
+++ CamaraX/settings.gradle.kts
@@ -19,6 +19,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "CamaraX"
+rootProject.name = "Camarax"
 include(":app")
  
